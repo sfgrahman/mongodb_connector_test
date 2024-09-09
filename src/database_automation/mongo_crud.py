1542bda467860c10b2f4a68939cc2cedce4ee3e7
sfgrahman <sfgrahman35@gmail.com>
2024-09-09 12:40:50 +0600

    test_5

--- src/database_automation/mongo_crud.py
+++ src/database_automation/mongo_crud.py
@@ -15,26 +15,26 @@ class mongo_operation:
         self.database_name=database_name
         self.collection_name=collection_name
        
-    def create_mongo_client(self,collection=None):
+    def create_mongo_client(self, collection=None):
         client=MongoClient(self.client_url)
         return client
     
-    def create_database(self,collection=None):
-        if mongo_operation.__database==None:
-            client=self.create_mongo_client(collection)
+    def create_database(self, collection = None):
+        if mongo_operation.__database == None:
+            client = self.create_mongo_client(collection)
             self.database=client[self.database_name]
         return self.database 
     
-    def create_collection(self,collection=None):
-        if mongo_operation.__collection==None:
+    def create_collection(self, collection = None):
+        if mongo_operation.__collection == None:
             database=self.create_database(collection)
             self.collection=database[self.collection_name]
-            mongo_operation.__collection=collection
+            mongo_operation.__collection = collection
         
-        if mongo_operation.__collection!=collection:
+        if mongo_operation.__collection != collection:
             database=self.create_database(collection)
-            self.collection=database[self.collection_name]
-            mongo_operation.__collection=collection
+            self.collection = database[self.collection_name]
+            mongo_operation.__collection = collection
             
         return self.collection
     
@@ -43,10 +43,10 @@ class mongo_operation:
             for data in record:
                 if type(data) != dict:
                     raise TypeError("record must be in the dict")    
-            collection=self.create_collection(collection_name)
+            collection = self.create_collection(collection_name)
             collection.insert_many(record)
-        elif type(record)==dict:
-            collection=self.create_collection(collection_name)
+        elif type(record) == dict:
+            collection = self.create_collection(collection_name)
             collection.insert_one(record)
     
     def bulk_insert(self,datafile, collection_name:str=None):
@@ -56,9 +56,12 @@ class mongo_operation:
             pd.read.csv(self.path, encoding = 'utf-8')
             
         elif self.path.endswith(".xlsx"):
-            dataframe=pd.read_excel(self.path,encoding = 'utf-8')
+            dataframe = pd.read_excel(self.path,encoding = 'utf-8')
             
-        datajson=json.loads(dataframe.to_json(orient = 'record'))
-        collection=self.create_collection()
+        datajson = json.loads(dataframe.to_json(orient = 'record'))
+        collection = self.create_collection()
         collection.insert_many(datajson)
+
+
+
 
