b18d25657b3c0e576cee0d259ca1f805b3693feb
sfgrahman <sfgrahman35@gmail.com>
2024-09-09 15:39:55 +0600

    fixes

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ with open('README.md', 'r', encoding='utf-8') as f:
     long_description = f.read()     
    
 
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 REPO_NAME = "mongodb_connector_test"
 PKG_NAME= "MongoConnectTestfinal"
 AUTHOR_USER_NAME = "sfgrahman"
